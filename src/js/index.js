4edef4aa2a6dba05f319763406ce5decccb9c739
Yaroslava Musiichuk <iaroslava1607@gmail.com>
2021-03-26 11:56:48 +0200

    makes edits

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,15 @@
 import menuItemsMarkupTpl from "../templates/menu-item-markup";
 import menu from "./menu.json";
+import refs from "./refs";
 import { addIngredientsOfDish } from "../js/add-ingredients-of-the-dish";
 
-const menuMarkup = document.querySelector(".js-menu");
-const body = document.querySelector("body");
-console.log(body);
 const cardsMenuMarkup = menuItemsMarkupTpl(menu);
 
-menuMarkup.insertAdjacentHTML("beforeend", cardsMenuMarkup);
+refs.menuMarkup.insertAdjacentHTML("beforeend", cardsMenuMarkup);
 
 const arrIngredientsOfDish = addIngredientsOfDish(menu);
-const listOfIngredients = document.querySelectorAll(".tag-list");
 
-listOfIngredients.forEach((dish, index) => {
+refs.listOfIngredients.forEach((dish, index) => {
   dish.insertAdjacentHTML("beforeend", arrIngredientsOfDish[index].join(""));
 });
 
@@ -21,26 +18,24 @@ const Theme = {
   DARK: "dark-theme",
 };
 
-const checkbox = document.querySelector("#theme-switch-toggle");
-
-checkbox.addEventListener("change", changeTheme);
+refs.checkbox.addEventListener("change", changeTheme);
 
 function changeTheme() {
-  if (body.classList.conteins(Theme.DARK)) {
-    body.classList.remove(Theme.DARK);
-    body.classList.add(Theme.LIGHT);
+  if (refs.body.classList.contains(Theme.DARK)) {
+    refs.body.classList.remove(Theme.DARK);
+    refs.body.classList.add(Theme.LIGHT);
     localStorage.setItem("theme", Theme.LIGHT);
   } else {
-    body.classList.remove(Theme.LIGHT);
-    body.classList.add(Theme.DARK);
+    refs.body.classList.remove(Theme.LIGHT);
+    refs.body.classList.add(Theme.DARK);
     localStorage.setItem("theme", Theme.DARK);
   }
 }
 
 if (localStorage.getItem("theme")) {
-  body.classList.add(localStorage.getItem("theme"));
+  refs.body.classList.add(localStorage.getItem("theme"));
 }
 
-if (body.classList.contains("dark-theme")) {
-  checkBox.checked = true;
+if (refs.body.classList.contains(Theme.DARK)) {
+  refs.checkbox.checked = true;
 }
---
+++ src/js/refs.js
@@ -0,0 +1,6 @@
+export default {
+  menuMarkup: document.querySelector(".js-menu"),
+  body: document.querySelector("body"),
+  listOfIngredients: document.querySelectorAll(".tag-list"),
+  checkbox: document.querySelector("#theme-switch-toggle"),
+};
